4e173de0c1d1dcd9533a8238a3cc31ca94196b8c
badalhalder99 <badalchandrahalder99@gmail.com>
2024-08-07 19:33:01 +0600

    first commit

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -3,12 +3,13 @@
 import { useEffect, useState } from 'react';
 import { signOut } from 'firebase/auth';
 import { auth } from '@/app/firebase/config'; 
-import Navbar from '@/components/Navbar';
+import styles from '@/components/navbar.module.css';
 import Footer from '@/components/Footer';
+import Link from 'next/link';
 
 const Dashboard = () => {
   const [user, setUser] = useState(null);
-
+  console.log(user)
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
       if (user) {
@@ -31,7 +32,24 @@ const Dashboard = () => {
 
   return (
     <div className="flex flex-col justify-between h-[100vh]">
-      <Navbar />
+      <header>
+        <nav className="bg-[#ddd] border-b border-solid border-[#dddddd28]">
+          <div
+            className={`container ${styles.customContainer} my-0 ml-auto mr-auto flex items-center justify-between py-[6px] md:px-5 2xsmall:px-4 lg:max-w-[1024px] xl:max-w-[1320px]`}
+          >
+            {/* Logo */}
+            <div>
+              <Link className="navbar-brand" href="/">
+                <img
+                  src="./logo.png"
+                  className="small:w-[150px] h-auto 2xsmall:w-[120px]"
+                />
+              </Link>
+            </div>
+            <div>{user.email.substring(0, 13)}</div>
+          </div>
+        </nav>
+      </header>
       <div className="container mx-auto">
         <div className="text-center mt-10">
           <h1 className="text-3xl font-bold mb-6">Welcome to Dashboard</h1>
